1cac7d7205b127eb3090cde369b6cb4a8f92c7ff
Ruka Nanda Vera <codeharuka.yusa@gmail.com>
2018-02-06 17:21:26 +0700

    configs: rename xrdb_get_color to xrgc

--- .bin/rofi-painter.sh
+++ .bin/rofi-painter.sh
@@ -7,10 +7,10 @@
 #
 
 source ${HOME}/.wmrc
-pallete="`xrdb_get_color bg`, `xrdb_get_color 7`, `xrdb_get_color bg`, `xrdb_get_color 7`, `xrdb_get_color bg`"
+pallete="`xrgc bg`, `xrgc 7`, `xrgc bg`, `xrgc 7`, `xrgc bg`"
 
 cat << EOF > $HOME/.cache/rofi.Xresources
-rofi.color-window: `xrdb_get_color bg`, `xrdb_get_color fg`
+rofi.color-window: `xrgc bg`, `xrgc fg`
 rofi.color-active: $pallete
 rofi.color-normal: $pallete
 rofi.color-urgent: $pallete
