840deb67aec66e27878e1fc108ca82ea650c4fac
Nanda Okitavera <codeharuka.yusa@gmail.com>
2018-11-01 09:58:55 +0000

    firefox: use user.js to fix invisible text insteads

---
+++ assets/.mozilla/firefox/profilename.default/user.js
@@ -0,0 +1,5 @@
+// Mozilla User Preferences
+// Fix invisible text on dark themes
+user_pref("widget.chrome.allow-gtk-dark-theme", true);
+user_pref("widget.content.allow-gtk-dark-theme", true);
+user_pref("widget.content.gtk-theme-override", "Adwaita"); // or any other light themes
