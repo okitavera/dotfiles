9c9d83413bf48a2d6e3affeaa2c71be68137aa08
Nanda Okitavera <codeharuka.yusa@gmail.com>
2018-11-11 12:51:52 +0000

    firefox: Fix HTML5 video tearing

--- assets/.mozilla/firefox/profilename.default/user.js
+++ assets/.mozilla/firefox/profilename.default/user.js
@@ -3,3 +3,5 @@
 user_pref("widget.chrome.allow-gtk-dark-theme", true);
 user_pref("widget.content.allow-gtk-dark-theme", true);
 user_pref("widget.content.gtk-theme-override", "Adwaita"); // or any other light themes
+// Fix HTML5 video tearing
+user_pref("layers.acceleration.force-enabled", true);
