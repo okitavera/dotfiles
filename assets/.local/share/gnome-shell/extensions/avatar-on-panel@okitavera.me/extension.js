d2807e976f8d52a55c64136cc10ba4fefffb6100
okitavera <codeharuka.yusa@gmail.com>
2019-03-15 14:38:15 +0700

    fix size doesn't reserved properly on gnome 3.32

--- assets/.local/share/gnome-shell/extensions/avatar-on-panel@okitavera.me/extension.js
+++ assets/.local/share/gnome-shell/extensions/avatar-on-panel@okitavera.me/extension.js
@@ -1,6 +1,5 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const avatarSize = Main.panel.actor.get_height() - 10;
 const userData = imports.gi.AccountsService.UserManager.get_default().get_user(
   imports.gi.GLib.get_user_name()
 );
@@ -11,24 +10,20 @@ let wrapper = null;
 var updateAva = () => {
   if (userData.get_icon_file() != null) {
     icon.style = `background-image: url("${userData.get_icon_file()}");`;
+    icon.width = icon.height = Main.panel.actor.get_height() - 8;
   }
 };
 
-var init = () => {
-  icon = new St.Bin({
-    style_class: "avatar-indicator",
-    width: avatarSize,
-    height: avatarSize
-  });
+var enable = () => {
+  icon = new St.Bin({ style_class: "avatar-indicator" });
   userData.connect("notify::is-loaded", updateAva.bind(this));
   userData.connect("changed", updateAva.bind(this));
   wrapper = new St.Bin({ style_class: "avatar-indicator--wrapper" });
   wrapper.add_actor(icon);
-  updateAva();
-};
-
-var enable = () =>
   Main.panel.statusArea.aggregateMenu._power.indicators.add_child(wrapper);
+  updateAva();
+}
+
 var disable = () => {
   Main.panel.statusArea.aggregateMenu._power.indicators.remove_child(wrapper);
   wrapper = null;
