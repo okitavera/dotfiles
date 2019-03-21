99898661446a51ab0fd7ac24770c30aa9f7e964c
okitavera <codeharuka.yusa@gmail.com>
2019-03-21 09:51:45 +0700

    some firefox security hardening

--- firefox/user.js
+++ firefox/user.js
@@ -1,4 +1,10 @@
 // Mozilla User Preferences
+user_pref("dom.webnotifications.enabled", false);
+user_pref("services.sync.engine.passwords", false);
+user_pref("security.ask_for_password", 0);
+user_pref("privacy.donottrackheader.enable", true);
+user_pref("privacy.resistFingerprintin", true);
+user_pref("geo.enabled", false);
 // Fix invisible text on dark themes
 user_pref("widget.chrome.allow-gtk-dark-theme", true);
 user_pref("widget.content.allow-gtk-dark-theme", true);
