8d4b1331fa692150c6a745483087e080d2b7431e
okitavera <codeharuka.yusa@gmail.com>
2019-03-22 19:12:22 +0700

    restore actual files instead of farming symlink

--- install.dots.sh
+++ install.dots.sh
@@ -5,17 +5,8 @@ log()(printf "${FUNCNAME[1]}: $1\n")
 
 deploy(){
   [[ -d "$1" ]] || { log "directory $1 doesn't exist. exiting"; exit 1; }
-  
-  mkdir -p "$2"
-  log "$1: checking conflicted files"
-  for i in $(stow -n "$1" -t "$2" 2>&1 | grep 'link nor' | sed -e 's/.*: //'); do
-    if [[ -f "$i" ]];then
-      log "$1: cleaning up $i"
-      rm "$2/$i"
-    fi
-  done
-  log "$1: installing to $2"
-  stow "$1" -t "$2"
+  log "copying modules $1 to $2"
+  cp -apf "$1/." "$2"
 }
 
 dotfox(){
@@ -36,7 +27,7 @@ dotfox(){
   
   MOZ=`awk '/\[/{prefix=$0; next} $1{print prefix $0}' "$2" | grep Path | sed -e 's/.*Path=//g'`
   log "copying modules to $HOME/.mozilla/firefox/$MOZ"
-  cp -arpf "$1" $HOME/.mozilla/firefox/$MOZ
+  cp -apf "$1/." $HOME/.mozilla/firefox/$MOZ
 }
 
 source $LIST
