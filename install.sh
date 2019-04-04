1a2f6891276e9a2bc9edf1a76751fe2f78b7cd8f
okitavera <codeharuka.yusa@gmail.com>
2019-04-05 00:02:32 +0700

    split the system and fonts install

--- install.sh
+++ install.sh
@@ -7,13 +7,3 @@ deploy bin $HOME/.local/bin
 deploy conky $HOME/.conky/okitavera-conky
 dotfox firefox $HOME/.mozilla/firefox/profiles.ini
 deploy vscode "$HOME/.config/Code - OSS/User"
-
-#-- Dangerous section!
-#-- Disabled by default 
-# SUDO=true deploy etc /etc
-
-#-- Fonts
-for i in DankMono-Italic DankMono-Regular
-do
-    gpg --decrypt fonts/$i.gpg > $HOME/.fonts/${i}.ttf
-done
