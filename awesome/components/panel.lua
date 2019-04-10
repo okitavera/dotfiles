f5e75b18f5ab83c838a814a1cc8b0aed74ae3f9e
okitavera <codeharuka.yusa@gmail.com>
2019-04-10 18:23:23 +0700

    awesome: localize widget and connect to tag update

--- awesome/components/panel.lua
+++ awesome/components/panel.lua
@@ -1,3 +1,5 @@
+local panel = { item = {} }
+
 awful.screen.connect_for_each_screen(function(scr)
   -- taglist a.k.a workspaces
   awful.tag({"ターミナル", "ミュージック", "ウェブ", "コード"}, scr, awful.layout.layouts[1])
@@ -17,7 +19,7 @@ awful.screen.connect_for_each_screen(function(scr)
     awful.button({}, 4, function(t) awful.tag.viewnext(t.screen) end),
     awful.button({}, 5, function(t) awful.tag.viewprev(t.screen) end)
   )
-  local p_taglist = awful.widget.taglist {
+  panel.item.taglist = awful.widget.taglist {
     screen = scr,
     buttons = btn_tag,
     filter = awful.widget.taglist.filter.all,
@@ -51,7 +53,7 @@ awful.screen.connect_for_each_screen(function(scr)
     awful.button({}, 4, function() awful.client.focus.byidx(1) end),
     awful.button({}, 5, function() awful.client.focus.byidx(-1) end)
   )
-  local p_tasklist = awful.widget.tasklist {
+  panel.item.tasklist = awful.widget.tasklist {
     screen = scr,
     filter = awful.widget.tasklist.filter.currenttags,
     buttons = btn_task,
@@ -65,8 +67,8 @@ awful.screen.connect_for_each_screen(function(scr)
             id = 'text_role',
             widget = wibox.widget.textbox
           },
-          left = 15,
-          right = 15,
+          left = 10,
+          right = 10,
           bottom = 2,
           widget  = wibox.container.margin
         },
@@ -79,34 +81,35 @@ awful.screen.connect_for_each_screen(function(scr)
     },
   }
   -- bar clock
-  local p_clock = {
+  panel.item.clock = {
     wibox.widget.textclock(" %I:%M %p "),
     layout = wibox.layout.fixed.horizontal
   }
+
   -- layouts
-  local p_left = {
-    p_taglist,
-    p_tasklist,
+  panel.left = {
+    panel.item.taglist,
     layout = wibox.layout.align.horizontal
   }
-  local p_middle = {
+  panel.middle = {
+    panel.item.tasklist,
     layout = wibox.layout.align.horizontal
   }
-  local p_right = {
-    p_clock,
+  panel.right = {
+    panel.item.clock,
     layout = wibox.layout.align.horizontal
   }
 
   -- setup main wibar
-  mywibar = awful.wibar {
+  panel.main = awful.wibar {
     position = "top",
     screen = scr
   }
-  mywibar:setup {
+  panel.main:setup {
     {
-      p_left,
-      p_middle,
-      p_right,
+      panel.left,
+      panel.middle,
+      panel.right,
       layout = wibox.layout.align.horizontal
     },
     left = 10,
@@ -117,11 +120,12 @@ end)
 
 -- dynamic wibar corner radius
 local wibar_shapemanager = function(c)
-  mywibar.shape = function(cr, w, h)
+  panel.main.shape = function(cr, w, h)
     if c.fullscreen or c.maximized then
       gears.shape.partially_rounded_rect(cr, w, h, false, false, true, true, 0)
     else
-      gears.shape.partially_rounded_rect(cr, w, h, false, false, true, true, beautiful.wibar_corner_radius or 10)
+      gears.shape.partially_rounded_rect(cr, w, h, false, false, true, true, 
+        beautiful.wibar_corner_radius or 10)
     end
   end
 end
@@ -129,4 +133,5 @@ end
 client.connect_signal("manage", function(c) wibar_shapemanager(c) end)
 client.connect_signal("request::geometry", function(c) wibar_shapemanager(c) end)
 client.connect_signal("request::activate", function(c) wibar_shapemanager(c) end)
+screen.connect_signal("tag::history::update", function(c) wibar_shapemanager(c) end)
 
