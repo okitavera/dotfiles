27195b357be1d9cd10bee161d36d6eb753280248
Nanda Okitavera <codeharuka.yusa@gmail.com>
2019-04-11 09:10:37 +0700

    vscode: move config folder

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ deploy themes $HOME/.themes
 deploy bin $HOME/.local/bin
 deploy conky $HOME/.conky/okitavera-conky
 dotfox firefox $HOME/.mozilla/firefox/profiles.ini
-deploy vscode "$HOME/.config/Code - OSS/User"
+deploy vscode "$HOME/.config/Code/User"
 deploy awesome $HOME/.config/awesome
