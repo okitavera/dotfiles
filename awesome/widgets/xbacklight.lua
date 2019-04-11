2246abc7bc485cb4e95b238804e12ec4e30b1645
Nanda Okitavera <codeharuka.yusa@gmail.com>
2019-04-11 18:29:20 +0700

    awesome: widgets: add xbacklight

---
+++ awesome/widgets/xbacklight.lua
@@ -0,0 +1,17 @@
+local awful = require("awful")
+local self = {}
+
+self.cmd = "xbacklight -get"
+self.widget = awful.widget.watch(self.cmd, 60, function(out)
+    local bright = string.gsub(out, "\n", "")
+    widget:set_text(" "..bright .. "%")
+end
+)
+self.update = function()
+  awful.spawn.easy_async(self.cmd, function(out)
+    local bright = string.gsub(out, "\n", "")
+    self.widget:set_text(" "..bright .. "%")
+  end)
+end
+
+return self
