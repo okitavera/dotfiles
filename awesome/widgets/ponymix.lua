e4ca7541dd94c4959e11eb7a493152587987cfe4
Nanda Okitavera <codeharuka.yusa@gmail.com>
2019-04-11 18:16:55 +0700

    awesome: update widget format

--- awesome/widgets/ponymix.lua
+++ awesome/widgets/ponymix.lua
@@ -1,16 +1,16 @@
 local awful = require("awful")
-local ponymix = {}
-ponymix.widget = awful.widget.watch("ponymix get-volume", 5,
+local self = {}
+self.widget = awful.widget.watch("ponymix get-volume", 5,
   function(widget, stdout)
     local vol = string.gsub(stdout, "\n", "")
     widget:set_text(" " .. vol .. "%")
   end
 )
-ponymix.update = function()
+self.update = function()
   awful.spawn.easy_async("ponymix get-volume", function(out)
     local vol = string.gsub(out, "\n", "")
-    ponymix.widget:set_text(" " .. vol .. "%")      
+    self.widget:set_text(" " .. vol .. "%")      
   end)
 end
 
-return ponymix
+return self
--- awesome/widgets/upower.lua
+++ awesome/widgets/upower.lua
@@ -1,5 +1,5 @@
 local awful = require("awful")
-local upower = {}
+local self = {}
 
 local get_percentage = function(out)
   for k, v in string.gmatch(out, '([%a]+[%a|-]+):%s*([%a|%d]+[,|%a|%d]-)') do
@@ -9,15 +9,15 @@ local get_percentage = function(out)
   end
 end
 
-upower.cmd = "upower -i /org/freedesktop/UPower/devices/battery_BAT0"
-upower.widget = awful.widget.watch(upower.cmd, 30, function(out)
+self.cmd = "upower -i /org/freedesktop/UPower/devices/battery_BAT0"
+self.widget = awful.widget.watch(self.cmd, 30, function(out)
     widget:set_text(" " .. get_percentage(out) .. "%")
   end
 )
-upower.update = function()
-  awful.spawn.easy_async(upower.cmd, function(out)
-    upower.widget:set_text(" " .. get_percentage(out) .. "%")
+self.update = function()
+  awful.spawn.easy_async(self.cmd, function(out)
+    self.widget:set_text(" " .. get_percentage(out) .. "%")
   end)
 end
 
-return upower
+return self
