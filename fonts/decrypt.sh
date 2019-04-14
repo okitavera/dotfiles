b35326406f66cebeaf1441eb885a649ae1df268f
Nanda Okitavera <codeharuka.yusa@gmail.com>
2019-04-14 23:52:05 +0700

    better fonts decrypt

--- fonts/decrypt.sh
+++ fonts/decrypt.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 SCRIPTPATH="$(readlink -f $0 | xargs dirname)"
-mkdir -p /tmp/dankmono
-for i in DankMono-Italic DankMono-Regular
-do
-    gpg --decrypt $SCRIPTPATH/$i.gpg > /tmp/dankmono/${i}.ttf
+DEST=/usr/share/fonts/TTF
+sudo mkdir -p "$DEST"
+for i in DankMono-Italic DankMono-Regular; do
+  gpg --decrypt "$SCRIPTPATH/$i.gpg" | sudo tee "$DEST/$i.ttf" >/dev/null
 done
+sudo fc-cache -vf
+fc-cache -vf
