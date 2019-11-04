46e715f41ab406ea0fd4d282eac2f73a5fc0a2a4
Nanda Oktavera <codeharuka.yusa@gmail.com>
2019-11-05 07:46:44 +0900

    gitconfig:  use ISO 8601-like format for one-line log
    Easy sorting, easy life
    
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>

--- restore-git-config.sh
+++ restore-git-config.sh
@@ -1,4 +1,4 @@
 git config --global alias.merge 'merge --no-ff'
-git config --global alias.onelog 'log --oneline --pretty=format:"%h # %ad %s" --date="format:%d-%m-%Y"'
+git config --global alias.onelog 'log --oneline --pretty=format:"%h # %ai %s"'
 git config --global merge.renamelimit 999999
 git config --global diff.renameLimit 999999
